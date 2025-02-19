97c2c1d0ade9623bf190a45e2c75c6cf37634640
Alan-Uni <alandiaz888@outlook.com>
2025-02-18 19:13:51 -0600

    first commit

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -1,7 +1,13 @@
-export const getlogin = (req: any, res: any) => { 
-    res.status(200).json({message: "Inico de sesion"});
-  }
-  
+
+
 export const getclose = (req: any, res: any) => {
-    res.status(200).json({message: "Cierre de sesion"});
-  }
+  const usuarios = [
+    { id: 1, nombre: "julio cesar", email: "julio34@gmail.com" },
+    { id: 2, nombre: "pedro pascal", email: "pedro56@gmail.com" },
+    { id: 3, nombre: "emilio tercero", email: "emilio3@gmail.com" }
+  ];
+  
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(usuarios, null, 2));
+};
+
--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { getlogin } from "../controllers/usuario.controller.ts";
 import { getclose } from "../controllers/usuario.controller.ts";
 
 const router = Router();
 
-router.get("/Ingreso", getlogin);
 router.get("/Cierre", getclose);
+
 
 export default router;
